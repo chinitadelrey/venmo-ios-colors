fd8d2a27b57de3076740ef3673b62f9aac964bdc
Dasmer Singh <dasmer.singh@venmo.com>
2015-02-18 13:37:16 -0800

    [UIColor+VenmoColorsSpec] Put view in it block

--- VenmoColorsTests/UIColor+VenmoColorsSpec.m
+++ VenmoColorsTests/UIColor+VenmoColorsSpec.m
@@ -2,14 +2,9 @@
 
 SpecBegin(UIColor_VenmoColors)
 
-__block UIView *view;
-
-beforeEach(^{
-    view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
-});
-
 describe(@"ven_venmoBlueColor", ^{
     it(@"should really be Venmo Blue™", ^{
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
         view.backgroundColor = [UIColor ven_venmoBlueColor];
         expect(view).to.haveValidSnapshot();
     });
