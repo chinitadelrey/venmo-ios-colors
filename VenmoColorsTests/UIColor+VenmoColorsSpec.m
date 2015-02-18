0d5aeff599231f435f71fdcbf7b47b64db9f56c3
Dasmer Singh <dasmer.singh@venmo.com>
2015-02-18 14:57:27 -0800

    Add test to ensure all category methods pass a valid hex string

--- VenmoColorsTests/UIColor+VenmoColorsSpec.m
+++ VenmoColorsTests/UIColor+VenmoColorsSpec.m
@@ -1,12 +1,34 @@
 #import <venmo-ios-colors/UIColor+VenmoColors.h>
+#import <Colours/Colours.h>
+#import "VENColorHelper.h"
 
 SpecBegin(UIColor_VenmoColors)
 
 describe(@"ven_venmoBlueColor", ^{
+
     it(@"should really be Venmo Blue™", ^{
         UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
         view.backgroundColor = [UIColor ven_venmoBlueColor];
         expect(view).to.haveValidSnapshot();
+    });
+});
+
+describe(@"colorWithHexString:", ^{
+
+    it(@"should only be passed strings that are valid 32 bit hexidecimal digits", ^{
+        id colorClassMock = [OCMockObject mockForClass:[UIColor class]];
+        NSString *hexRegex = @"^([A-Fa-f0-9]{6})$";
+
+        for (NSString *selectorName in [VENColorHelper venmoColors]) {
+            [[colorClassMock expect] colorFromHexString:[OCMArg checkWithBlock:^BOOL(NSString *hexString) {
+                NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", hexRegex];
+                return [predicate evaluateWithObject:hexString];
+            }]];
+
+            [[UIColor class] performSelector:NSSelectorFromString(selectorName)];
+
+            [colorClassMock verify];
+        }
     });
 });
 
