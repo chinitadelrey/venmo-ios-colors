ce079ee4fd50028d9aa9508691f15800485675ea
Dasmer Singh <dasmer.singh@venmo.com>
2015-02-18 13:00:26 -0800

    Use snapshot tests to test Venmo Blue

--- Podfile
+++ Podfile
@@ -2,7 +2,8 @@ target 'VenmoColorsSample' do
   pod 'venmo-ios-colors', :path => '.'
 end
 
-target 'VenmoColorsTests' do
+target 'VenmoColorsSampleTests' do
   pod 'Specta', '~> 0.3'
   pod 'Expecta', '~> 0.3'
+  pod 'Expecta+Snapshots', '~> 1.3'
 end
--- VenmoColorsTests/UIColor+VenmoColorsSpec.m
+++ VenmoColorsTests/UIColor+VenmoColorsSpec.m
@@ -1,16 +1,17 @@
 #import <venmo-ios-colors/UIColor+VenmoColors.h>
-#import <Colours/Colours.h>
-#import <Specta/Specta.h>
-#import <Expecta/Expecta.h>
 
 SpecBegin(UIColor_VenmoColors)
 
+__block UIView *view;
+
+beforeEach(^{
+    view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+});
+
 describe(@"ven_venmoBlueColor", ^{
     it(@"should really be Venmo Blue™", ^{
-        UIColor *venmoBlue = [UIColor ven_venmoBlueColor];
-        expect([venmoBlue red]).to.equal(61/255.0f);
-        expect([venmoBlue green]).to.equal(149/255.0f);
-        expect([venmoBlue blue]).to.equal(206/255.0f);
+        view.backgroundColor = [UIColor ven_venmoBlueColor];
+        expect(view).to.haveValidSnapshot();
     });
 });
 
